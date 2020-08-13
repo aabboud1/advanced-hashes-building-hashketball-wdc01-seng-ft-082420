5e38eed8e1b64170a7691f239cdf4b4b96c26e31
Ahmed Abboud <abboudahmed1@gmail.com>
2020-08-13 18:23:31 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,10 +2,20 @@
 def game_hash
   {
   :home =>{
-    :team_name => {
+    :team_name => "Brooklyn Nets"
+    :colors => ["Black", "White"]
+    :players => [
+      :player_name => {}
+      :number => {}
+      :shoe => {}
+      :points => {}
+      :rebounds => {}
+      :assists => {}
+      :steals => {}
+      :blocks => {}
+      :slam_dunks => {}
       
-    }
-    colors
+      ]
     
     
   }
